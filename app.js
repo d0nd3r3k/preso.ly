b5c660cd3aea3d3217667320a9e45a1d817b2b8e
DonaldDerek <donaldderek.haddad@gmail.com>
2013-05-26 10:58:55 +0200

    deb

--- app.js
+++ app.js
@@ -32,7 +32,8 @@ io.set('transports', [
 io.set('authorization', function (data, accept) {
     return accept(null, true);  
 }).sockets.on('connection', function(socket){
-    
+    console.log(socket);
+    console.log("Foo");
     socket.on('sync',function(data){
         console.log(data);
         if(data.type == "screen"){
